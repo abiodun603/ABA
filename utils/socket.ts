e9b3722805e41571d0b2b952c51a17237d1c1d99
abidountelvida <abiodun.olatunji@telvida.com>
2023-10-09 16:53:45 +0100

    chat integration

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io("https://5e31-102-88-62-142.ngrok-free.app")
+const socket = io("https://5002-102-88-62-100.ngrok-free.app")
 export default socket;
