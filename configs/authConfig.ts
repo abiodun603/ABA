00c615dae4e6bec8485def4ffc8fca3cc7f004cf
abidountelvida <abiodun.olatunji@telvida.com>
2024-01-11 09:15:08 +0100

    update url

--- configs/authConfig.ts
+++ configs/authConfig.ts
@@ -2,7 +2,7 @@ import {  fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../stores/store';
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: "https://5f17-102-221-45-187.ngrok-free.app/api",
+  baseUrl: "https://5700-102-88-81-13.ngrok-free.app/api",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
--- utils/socket.ts
+++ utils/socket.ts
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io("https://5f17-102-221-45-187.ngrok-free.app")
+const socket = io("https://5700-102-88-81-13.ngrok-free.app")
 export default socket;
