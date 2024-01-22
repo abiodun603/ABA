0b6b2060338dfa21d845884bab77101063ae34ce
abidountelvida <abiodun.olatunji@telvida.com>
2024-01-22 14:12:28 +0100

    git push

--- configs/authConfig.ts
+++ configs/authConfig.ts
@@ -2,7 +2,7 @@ import {  fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../stores/store';
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: `https://3f54-105-112-20-182.ngrok-free.app/api`,
+  baseUrl: `https://a16a-197-211-58-105.ngrok-free.app/api`,
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
